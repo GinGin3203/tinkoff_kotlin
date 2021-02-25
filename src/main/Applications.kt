4994c63a1f3705ea6dd66c5d77f3ec3c8e174187
GinGin3203 <ginzburggeorgy@gmail.com>
2021-02-26 01:27:39 +0300

    changed method declarations to shorter ones

--- src/main/Applications.kt
+++ src/main/Applications.kt
@@ -6,27 +6,19 @@ abstract class Application {
     protected abstract val purpose: String
     protected abstract val features: HashSet<String>
 
-    open fun about() {
-        println(getDescription())
-    }
+    open fun about() = println(getDescription())
 
-    fun getDescription(): String {
-        return "Hi! My name is $name, I am $applicationType. My primary purpose is $purpose."
-    }
 
-    fun feature(feature: String) {
-        if (feature !in features) {
-            println("Sorry, I don't have this feature :(")
-            return
-        }
+    fun getDescription(): String = "Hi! My name is $name, I am $applicationType. My primary purpose is $purpose."
 
-        println("Yep, I can do that! $feature is one of the best things about me!")
 
-    }
+    fun feature(feature: String) = if (feature !in features) println(
+            "Sorry, I don't have this feature :(")
+    else println("Yep, I can do that! $feature is one of the best things about me!")
 
-    fun feature() {
-        println("My features include but are not limited to: $features")
-    }
+
+    fun feature() = println("My features include but are not limited to: $features")
+
 
 }
 
@@ -34,49 +26,47 @@ class TextEditor(override val name: String, override val features: HashSet<Strin
     override val applicationType: String = "text editor"
     override val purpose: String = "editing text"
 
-    fun makeCoolEdit(text: String): String {
-        return text.reversed()
-    }
+    fun makeCoolEdit(text: String): String = text.reversed()
+
 }
 
 class MediaGallery(
-    override val name: String,
-    private val supportedFileFormat: String,
-    override val features: HashSet<String>
+        override val name: String,
+        private val supportedFileFormat: String,
+        override val features: HashSet<String>
 ) : Application() {
     override val applicationType: String = "media gallery"
     override val purpose: String = "viewing media"
     private val seed: Int = 198274
 
-    override fun about() {
-        println(getDescription() + " I only support files in $supportedFileFormat format")
-    }
+    override fun about() = println(getDescription() +
+            " I only support files in $supportedFileFormat format")
 
-    fun shufflePeopleOnPhoto(people: List<String>): List<String> {
-        return people.shuffled(Random(seed))
-    }
+
+    fun shufflePeopleOnPhoto(people: List<String>): List<String> = people.shuffled(Random(seed))
 
 
 }
 
 class IDE(override val name: String, private val primaryLang: String, override val features: HashSet<String>) :
-    Application() {
+        Application() {
     override val applicationType: String = "IDE"
     override val purpose: String = "coding"
 
-    override fun about() {
-        println(getDescription() + " I was created primarily for the $primaryLang programming language.")
-    }
+    override fun about() = println(getDescription() +
+            " I was created primarily for the $primaryLang programming language.")
+
 
     fun showAnnoyingError() {
         val randomErrorCode = getRandomString((3..5).random())
-        println("An annoying error occurred in your code! Error code: ${primaryLang.toUpperCase()}_ERROR_0x$randomErrorCode")
+        println("An annoying error occurred in your code! Error code: " +
+                "${primaryLang.toUpperCase()}_ERROR_0x$randomErrorCode")
     }
 
     private fun getRandomString(string_length: Int): String {
         val allowedChars = ('A'..'F') + ('0'..'9')
         return (1..string_length)
-            .map { allowedChars.random() }
-            .joinToString("")
+                .map { allowedChars.random() }
+                .joinToString("")
     }
 }
--- src/main/main.kt
+++ src/main/main.kt
@@ -6,16 +6,19 @@ import IDE
 
 
 fun main() {
+
+    // Creating objects
     val vim = TextEditor("Vim", hashSetOf("modular", "extensible"))
 
     val windowsMediaViewer = MediaGallery(
-        "Windows Media Viewer",
-        "jpg", hashSetOf("ease of use")
+            "Windows Media Viewer",
+            "jpg", hashSetOf("ease of use")
     )
 
     val idea = IDE("IntelliJ IDEA", "Java", hashSetOf("cool autocompletion", "convenience"))
 
 
+    // TextEditor functionality
     vim.about()
     println(vim.makeCoolEdit("I like ice cream"))
     vim.feature()
@@ -23,10 +26,12 @@ fun main() {
     vim.feature("easy for newcomers")
     println()
 
+    // MediaGallery functionality
     windowsMediaViewer.about()
     println(windowsMediaViewer.shufflePeopleOnPhoto(listOf("Vasya", "Petya", "Katya", "Sasha", "Andrei")))
     println()
 
+    // IDE functionality
     idea.about()
     idea.showAnnoyingError()
 }
