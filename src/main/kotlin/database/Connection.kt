a9a48a908f5fde2d58f0a879b84ac9dcb598c6d3
GinGin3203 <ginzburggeorgy@gmail.com>
2021-03-24 02:33:18 +0300

    reworked join and selectById methods

--- src/main/kotlin/database/Connection.kt
+++ src/main/kotlin/database/Connection.kt
@@ -1,7 +1,9 @@
 package database
 
 import domain_entities.*
+import java.lang.IllegalArgumentException
 import java.sql.DriverManager
+import java.sql.ResultSet
 import java.sql.Types
 
 data class Connection(val db_url: String) {
@@ -32,35 +34,64 @@ data class Connection(val db_url: String) {
         }
     }
 
-    fun selectById(tableName: String, id: Int, condition: Char): List<Application> {
+    private fun buildRawResponseFromResultSet(rs: ResultSet): List<Map<String, Any?>> {
+        val retList = ArrayList<HashMap<String, Any?>>()
+        val md = rs.metaData
+        while (rs.next()) {
+            val resMap = HashMap<String, Any?>()
+            for (i in 1..md.columnCount) {
+                val type: Int = md.getColumnType(i)
+                if (type == Types.VARCHAR || type == Types.CHAR) {
+                    resMap[md.getColumnName(i)] = rs.getString(i)
+                } else {
+                    resMap[md.getColumnName(i)] = rs.getInt(i)
+                }
+            }
+            retList.add(resMap)
+        }
+        return retList
+    }
+
+    fun selectById(tableName: String, id: Int, condition: Char): List<Map<String, Any?>> {
 
         val conn = DriverManager.getConnection(db_url)
-        val retList = ArrayList<Application>()
         conn.use {
             val sql = ScriptsManager.selectByIdScriptTemplate.format(tableName, condition)
             val ps = it.prepareStatement(sql)
             ps.setInt(1, id)
             val rs = ps.executeQuery()
-            val md = rs.metaData
-            while (rs.next()) {
-                val resMap = HashMap<String, Any?>()
-                for (i in 1..md.columnCount) {
-                    val type: Int = md.getColumnType(i)
-                    if (type == Types.VARCHAR || type == Types.CHAR) {
-                        resMap[md.getColumnName(i)] = rs.getString(i)
-                    } else {
-                        resMap[md.getColumnName(i)] = rs.getInt(i)
-                    }
-                }
-                if (tableName == "Ide")
-                    retList.add(ideOf(resMap))
-                else if (tableName == "MediaViewer")
-                    retList.add(mediaViewerOf(resMap))
-                else if (tableName == "TextEditor")
-                    retList.add(textEditorOf(resMap))
-            }
+            return buildRawResponseFromResultSet(rs)
         }
-        return retList
+    }
+
+
+    private enum class JoinType {
+        INNER, LEFT
+    }
+
+    private enum class LeftTable {
+        IDE, TEXTEDITOR
+    }
+
+    private fun convertJoinType(joinType: String) =
+        try {
+            val jt = JoinType.valueOf(joinType)
+            jt
+        } catch (e: IllegalArgumentException) {
+            println("joinType argument is incorrect. Available values: INNER or LEFT")
+            null
+        }
+
+    fun join(joinType: String, leftTable: String): List<Map<String, Any?>>? {
+        val conn = DriverManager.getConnection(db_url)
+        conn.use {
+            val lt = LeftTable.valueOf(leftTable)
+            val jt = convertJoinType(joinType) ?: return null
+            val sql = if (lt == LeftTable.IDE) ScriptsManager.joinIdeAndTextEditorScript.format(jt) else
+                ScriptsManager.joinTextEditorAndIdeScript.format(jt)
+            val rs = it.prepareStatement(sql).executeQuery()
+            return buildRawResponseFromResultSet(rs)
+        }
     }
 
     fun clean() {
