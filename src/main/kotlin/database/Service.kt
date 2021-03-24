299b3b08fae28cfcf06dd697981d2c2b392c8026
GinGin3203 <ginzburggeorgy@gmail.com>
2021-03-24 05:33:34 +0300

    methods reworked

--- src/main/kotlin/database/Service.kt
+++ src/main/kotlin/database/Service.kt
@@ -1,16 +1,22 @@
 package database
 
+import domain_entities.Application
+import domain_entities.TableClasses
+import domain_entities.applicationOf
+
 
 class Service(private val connection: Connection) {
 
-    fun selectFirst(tableName: String) = connection.selectById(tableName, 1, '=').first()
+    fun selectFirst(tableName: TableClasses): Application =
+        applicationOf(connection.selectById(tableName, 1, '=').first())
 
-    fun selectAllWhichHaveLargerId(tableName: String, id: Int) = connection.selectById(tableName, id, '>')
+    fun selectAllWhichHaveLargerId(tableName: TableClasses, id: Int): List<Application> =
+        connection.selectById(tableName, id, '>').map { applicationOf(it) }
 
-    enum class JoinType {
-        INNER, LEFT
-    }
+    fun textEditorAndIdeJoin(joinType: JoinType): List<Application> =
+        connection.join(joinType, LeftTable.TextEditor).map { applicationOf(it) }
 
-//    fun textEditorAndIdeJoin(joinType: String) = connection.join(JoinType.valueOf(joinType))
-//    fun ideAndTextEditorJoin(joinType: String) = connection.join(JoinType.valueOf(joinType))
+    fun ideAndTextEditorJoin(joinType: JoinType): List<Application> =
+        connection.join(joinType, LeftTable.Ide).map { applicationOf(it) }
+
 }
