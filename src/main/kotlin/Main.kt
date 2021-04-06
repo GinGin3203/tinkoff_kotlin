19212de384b10fa630ebe1be2032dfdcb6b4bb2c
GinGin3203 <ginzburggeorgy@gmail.com>
2021-04-06 20:03:13 +0300

    cleanup

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,4 @@
-import domain_entities.AuthorAndBooks
-import kotlinx.coroutines.async
-import kotlinx.coroutines.runBlocking
-import services.AuthorInfoProvider
-import services.AvailableBooksProvider
-
-fun main() = runBlocking {
-
-    val authorsDef = async { AuthorInfoProvider.getAllData() }
-    val booksDef = async { AvailableBooksProvider.getAllData() }
-
-    val authors = authorsDef.await()
-    val books = booksDef.await()
-
-    val joinedData = authors.mapNotNull { author ->
-        books.firstOrNull { it.authorName == author.name }?.let { authorBooksData ->
-            AuthorAndBooks(
-                author.name, author.yearOfBirth, authorBooksData.books
-            )
-        }
-    }
-
-    println(joinedData)
+fun main() {
+    // Task 1
+    getConcurrentlyAndJoin()
 }
---
+++ src/main/kotlin/Task1.kt
@@ -0,0 +1,24 @@
+import domain_entities.AuthorAndBooks
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+import services.AuthorInfoProvider
+import services.AvailableBooksProvider
+
+fun getConcurrentlyAndJoin() = runBlocking {
+
+    val authorsDef = async { AuthorInfoProvider.getAllData() }
+    val booksDef = async { AvailableBooksProvider.getAllData() }
+
+    val authors = authorsDef.await()
+    val books = booksDef.await()
+
+    val joinedData = authors.mapNotNull { author ->
+        books.firstOrNull { it.authorName == author.name }?.let { authorBooksData ->
+            AuthorAndBooks(
+                author.name, author.yearOfBirth, authorBooksData.books
+            )
+        }
+    }
+    println(joinedData)
+}
+
